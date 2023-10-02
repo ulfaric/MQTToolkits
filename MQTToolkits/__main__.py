4e5d7c540d66b8a92c2be2acbdccbcc2aa5d68c2
ulfaric <ryf0510@live.com>
2023-10-02 15:14:38 +0800

    add comments

--- MQTToolkits/__main__.py
+++ MQTToolkits/__main__.py
@@ -23,8 +23,48 @@ class ContentType(str, enum.Enum):
     STRING = "string"
 
 
+from typing import Any
+from datetime import datetime
+from paho.mqtt.client import Client
+
 class Publisher:
+    """
+    A class representing a MQTT publisher.
+
+    Attributes:
+    -----------
+    name : str
+        The name of the publisher.
+    topic : str
+        The topic to which the publisher publishes messages.
+    mqtt_client : Client
+        The MQTT client used by the publisher.
+    _stop : bool
+        A flag indicating whether the publisher should stop publishing messages.
+    _created_at : datetime
+        The datetime when the publisher was created.
+    _published_messages : int
+        The number of messages published by the publisher.
+
+    Methods:
+    --------
+    publish(payload: str) -> None
+        Publishes a message to the topic.
+    """
+
     def __init__(self, name: str, topic: str, mqtt_client: Client) -> None:
+        """
+        Initializes a new instance of the Publisher class.
+
+        Parameters:
+        -----------
+        name : str
+            The name of the publisher.
+        topic : str
+            The topic to which the publisher publishes messages.
+        mqtt_client : Client
+            The MQTT client used by the publisher.
+        """
         self._name = name
         self._topic = topic
         self._mqtt_client = mqtt_client
@@ -32,30 +72,52 @@ class Publisher:
         self._created_at = datetime.now()
         self._published_messages = 0
 
-    def publish(self, payload: str):
+    def publish(self, payload: str) -> None:
+        """
+        Publishes a message to the topic.
+
+        Parameters:
+        -----------
+        payload : str
+            The message to publish.
+        """
         self._mqtt_client.publish(self.topic, payload)
         self._published_messages += 1
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """
+        Gets the name of the publisher.
+        """
         return self._name
 
     @property
-    def topic(self):
+    def topic(self) -> str:
+        """
+        Gets the topic to which the publisher publishes messages.
+        """
         return self._topic
 
     @property
-    def mqtt_client(self):
+    def mqtt_client(self) -> Client:
+        """
+        Gets the MQTT client used by the publisher.
+        """
         return self._mqtt_client
 
     @property
-    def lifetime(self):
+    def lifetime(self) -> Any:
+        """
+        Gets the lifetime of the publisher.
+        """
         return datetime.now() - self._created_at
 
     @property
-    def published_messages(self):
+    def published_messages(self) -> int:
+        """
+        Gets the number of messages published by the publisher.
+        """
         return self._published_messages
-
 
 class Toolkit:
     def __init__(self) -> None:
@@ -75,7 +137,15 @@ class Toolkit:
         password: str,
         client_id: str | None = None,
     ):
-        """Connect to the MQTT broker."""
+        """Connect to the MQTT broker.
+
+        Args:
+            host (str): The hostname or IP address of the MQTT broker.
+            port (int): The port number of the MQTT broker.
+            username (str): The username for the MQTT broker.
+            password (str): The password for the MQTT broker.
+            client_id (str, optional): The client ID for the MQTT broker. Defaults to None.
+        """
         self._host = host
         self._port = port
         self._username = username
@@ -89,8 +159,15 @@ class Toolkit:
             raise Exception(f"{e}\nFailed to connect to the MQTT broker.")
 
     def createPublisher(self, name: str | None, topic: str):
-        """Create a publisher."""
+        """Create a publisher.
 
+        Args:
+            name (str, optional): The name of the publisher. Defaults to None.
+            topic (str): The topic to publish to.
+
+        Raises:
+            Exception: If the publisher name already exists.
+        """
         publisher = Publisher(name or str(uuid.uuid4().int), topic, self.client)
         if publisher.name in [p.name for p in self.publishers]:
             raise Exception("The publisher name already exists.")
@@ -98,7 +175,11 @@ class Toolkit:
         return publisher
 
     def deletePublisher(self, name: str):
-        """Delete a publisher thread."""
+        """Delete a publisher thread.
+
+        Args:
+            name (str): The name of the publisher to delete.
+        """
         for publisher in self.publishers:
             if publisher.name == name:
                 publisher._stop = True
@@ -107,30 +188,37 @@ class Toolkit:
 
     @property
     def host(self):
+        """The hostname or IP address of the MQTT broker."""
         return self._host
 
     @property
     def port(self):
+        """The port number of the MQTT broker."""
         return self._port
 
     @property
     def username(self):
+        """The username for the MQTT broker."""
         return self._username
 
     @property
     def password(self):
+        """The password for the MQTT broker."""
         return self._password
 
     @property
     def client_id(self):
+        """The client ID for the MQTT broker."""
         return self._client_id
 
     @property
     def client(self):
+        """The MQTT client instance."""
         return self._client
 
     @property
     def publishers(self):
+        """The list of publishers."""
         return self._publishers
 
 
@@ -152,7 +240,16 @@ def create(
         ContentType, typer.Option(help="The content type of the payload")
     ] = ContentType.STRING,
 ):
-    """Create a publisher. The publisher will continously publish the payload to the topic with the given period."""
+    """
+    Create a publisher that continuously publishes the payload to the topic with the given period.
+
+    Args:
+        name (str): The name of this publish thread.
+        topic (str): The name of the topic.
+        payload (str): The payload to be published.
+        period (float, optional): The period for publishing the payload. Defaults to 1.
+        content_type (ContentType, optional): The content type of the payload. Defaults to ContentType.STRING.
+    """
     try:
         publisher = toolkit.createPublisher(name, topic)
     except Exception as e:
@@ -195,7 +292,11 @@ def create(
 
 @publisher.command("list")
 def list():
-    """List all the publishers."""
+    """
+    List all the publishers.
+
+    This function prints a table of all the publishers in the toolkit, including their name, topic, lifetime, and number of sent messages.
+    """
     table = Table("Name", "Topic", "Life", "Sent Messages", title="Publishers")
     console = Console()
     for publisher in toolkit.publishers:
@@ -212,12 +313,31 @@ def list():
 def delete(
     name: Annotated[str, typer.Argument(help="The name of the publisher thread.")]
 ):
-    """Delete a publisher. The publisher will stop publishing the payload."""
+    """
+    Delete a publisher.
+
+    This function deletes a publisher with the given name. The publisher will stop publishing the payload.
+
+    Args:
+        name (str): The name of the publisher thread.
+
+    Returns:
+        None
+    """
     toolkit.deletePublisher(name)
 
 
 @app.callback(invoke_without_command=True)
 def launch(ctx: Context):
+    """
+    Launches the MQTT Injector tool by connecting to the MQTT broker and starting the command line interface.
+
+    Args:
+        ctx (Context): The context object for the CLI.
+
+    Returns:
+        None
+    """
     while True:
         host = os.getenv("MQTT_HOST")
         if host is None:
